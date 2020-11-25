bcbe5e341429ceb43b6b71d601c96013c5f2f860
thenatural86 <philip.smalls86@gmail.com>
2020-11-24 19:46:55 -0500

    state basics

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React from 'react'
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
-};
+  let title = 'random title'
+  const handleClick = () => {
+    title = 'yolo time'
+    console.log(title)
+  }
 
-export default ErrorExample;
+  return (
+    <>
+      <h2>{title}</h2>
+      <button type='button' className='btn' onClick={handleClick}>
+        Yolo Button
+      </button>
+    </>
+  )
+}
+
+export default ErrorExample
