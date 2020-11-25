7efa691898d159309cf2e5c8fce93b1c49029d2f
thenatural86 <philip.smalls86@gmail.com>
2020-11-25 12:39:21 -0500

    update objects with spread op

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
-};
+  const [person, setPerson] = useState({
+    name: 'pete',
+    age: 24,
+    message: 'rando mess',
+  })
+  // console.log(person)
+  const { name, age, message } = person
 
-export default UseStateObject;
+  const changeMessage = () => {
+    setPerson({ ...person, message: 'yolo world' })
+  }
+  return (
+    <>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>{message}</h3>
+      <button className='btn' onClick={changeMessage}>
+        Yolo Button
+      </button>
+    </>
+  )
+}
+
+export default UseStateObject
