9950eb9e488b5cb099889d125a132e96f487e580
thenatural86 <philip.smalls86@gmail.com>
2020-11-26 12:43:51 -0500

    change

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
-};
+  console.log('hello')
+  return <h2>useEffect Basics</h2>
+}
 
-export default UseEffectBasics;
+export default UseEffectBasics
